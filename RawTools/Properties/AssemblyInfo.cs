4e95a3dcc68cbe09428d0e4de11a7c619e91c71b
Kevin Kovalchik <kevin.kovalchik@gmail.com>
2018-10-03 14:12:38 -0700

    update build

--- RawTools/Properties/AssemblyInfo.cs
+++ RawTools/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.10.2018.10")]
+[assembly: AssemblyFileVersion("3.10.2018.0")]
 
 [assembly: AssemblyInformationalVersion("1.2.0")]
